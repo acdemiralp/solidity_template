6db4b0d810f327e269d2c9fb9c8623bfcd4f72fb
acdemiralp <demiralpali@gmail.com>
2021-04-14 20:47:42 +0200

    Updated readme + formatting fixes.

--- .template/contracts/Main.sol
+++ .template/contracts/Main.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Main is ERC20 
-{
-    constructor() ERC20("Main", "MT") 
-    {
+contract Main is ERC20 {
+    constructor() ERC20("Main", "MT") {
         _mint(msg.sender, 1000);
     }
 
