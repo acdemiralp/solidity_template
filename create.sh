8b8ebc76f33ec8e65f1912f7aeec195744974adc
acdemiralp <demiralpali@gmail.com>
2021-04-14 20:47:13 +0200

    Create script now removes the template folder and itself.

--- create.sh
+++ create.sh
@@ -2,4 +2,7 @@
 /bin/bash npm init
 /bin/bash npm install --save ganache-cli truffle @chainlink/contracts @openzeppelin/contracts@latest @truffle/hdwallet-provider @uniswap/v2-core @uniswap/v2-periphery
 /bin/bash npx truffle init
-/bin/bash cp ./.template/* .
+mv .template/* .
+rm -rf .template
+rm create.bat
+rm -- "$0"
